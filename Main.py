08f62ac3a06898e4246c765e94484f827074db09
danielwboyce <danielwboyce@gmail.com>
2019-11-15 14:23:44 -0700

    Fundamentally working now. Adding documentation.

--- Main.py
+++ Main.py
@@ -1,18 +1,24 @@
 from ReportCard import *
 
-
+# The files where the data are stored
 students_filename = '/home/daniel/Downloads/backend-assessment/backend-assessment/students.csv'
-courses_filename  = '/home/daniel/Downloads/backend-assessment/backend-assessment/courses.csv'
-marks_filename    = '/home/daniel/Downloads/backend-assessment/backend-assessment/marks.csv'
-tests_filename    = '/home/daniel/Downloads/backend-assessment/backend-assessment/tests.csv'
+courses_filename = '/home/daniel/Downloads/backend-assessment/backend-assessment/courses.csv'
+marks_filename = '/home/daniel/Downloads/backend-assessment/backend-assessment/marks.csv'
+tests_filename = '/home/daniel/Downloads/backend-assessment/backend-assessment/tests.csv'
 
-students_by_id =  import_data_as_map_of_maps(students_filename, "id")
-courses_by_id = import_data_as_map_of_maps(courses_filename, "id")
-marks_by_student_id = import_data_as_map_of_maps(marks_filename, "test_id")
-tests_by_id = import_data_as_map_of_maps(tests_filename, "id")
+# Conversion from .csv files into maps of various structures. Further explanation of their structure is given in ReportCard.py and README.md
+students_map = import_students_as_map(students_filename)
+courses_map = import_courses_as_map_of_arrays(courses_filename)
+marks_map = import_marks_as_map_of_maps(marks_filename)
+tests_map = import_tests_as_map_of_arrays(tests_filename)
 
-print(marks_by_test_id)
-#print(students['1']['name'])
+# This function calculates the score the student got in each course and stores it in a map of maps.
+student_scores = generate_course_scores(students_map, marks_map, tests_map)
+
+# This function calculates each student's average score across all their courses
+student_averages = calculate_students_averages(student_scores)
+
+print_report_card(student_scores, student_averages, students_map, courses_map)
 
 
 
--- ReportCard.py
+++ ReportCard.py
@@ -11,9 +11,82 @@ def import_data_as_map_of_maps(filename, new_key_parameter):
     return map_of_maps
 
 
-def generate_report_cards(students, courses, marks, tests):
+def import_students_as_map(filename):
+    students_map = {}
+    with open(filename) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            students_map[row["id"]] = row["name"]
+    return students_map
+
+def import_marks_as_map_of_maps(filename):
+    marks_map = {}
+    with open(filename) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            if row["student_id"] not in marks_map:
+                marks_map[row["student_id"]] = {}
+                marks_map[row["student_id"]][row["test_id"]] = row["mark"]
+            else:
+                marks_map[row["student_id"]][row["test_id"]] = row["mark"]
+    return marks_map
+
+
+def import_tests_as_map_of_arrays(filename):
+    tests_map = {}
+    with open(filename) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            tests_map[row["id"]] = [row["course_id"], row["weight"]]
+    return tests_map
+
+
+def import_courses_as_map_of_arrays(filename):
+    courses_map = {}
+    with open(filename) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            courses_map[row["id"]] = [row["name"], row["teacher"]]
+    return courses_map
+
+
+def generate_course_scores(students, marks, tests):
     map_of_students_raw_grades = {}
     for student_id in students:
         map_of_students_raw_grades[student_id] = {}
-    for test_id in tests:
-        map_of_students_raw_grades[tests[test_id]["course_id"]] = tests[test_id]
+    for student_id in marks:
+        student_marks = marks[student_id]
+        for test_id in student_marks:
+            course_id = tests[test_id][0]
+            mark = float(student_marks[test_id])
+            weight = float(tests[test_id][1])
+            weighted_score = (weight / 100) * mark
+
+            if course_id not in map_of_students_raw_grades[student_id]:
+                map_of_students_raw_grades[student_id][course_id] = weighted_score
+            else:
+                map_of_students_raw_grades[student_id][course_id] += weighted_score
+    return map_of_students_raw_grades
+
+
+def calculate_students_averages(student_scores):
+    student_averages = {}
+    for student_id in student_scores:
+        student_num_of_courses = len(student_scores[student_id].values())
+        student_average = sum(student_scores[student_id].values()) / student_num_of_courses
+        student_averages[student_id] = student_average
+    return student_averages
+
+def print_report_card(student_scores, student_averages, students, courses):
+    for student_id in student_scores:
+        student_name = students[student_id]
+        student_average = student_averages[student_id]
+        student_header_string = f'Student Id: {student_id}, name: {student_name}\nTotal Average:\t{student_average:2.2f}\n'
+        print(student_header_string)
+        for course_id in student_scores[student_id]:
+            course_name = courses[course_id][0]
+            teacher_name = courses[course_id][1]
+            course_grade = student_scores[student_id][course_id]
+            course_string = f'\tCourse: {course_name}, Teacher: {teacher_name}\n\tFinal Grade:\t{course_grade:2.2f}\n'
+            print(course_string)
+
